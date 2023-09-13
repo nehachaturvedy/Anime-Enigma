a2d810763739d84df97e779fe54f1bdf54cca0fa
Neha <nehachaturvedi990@gmail.com>
2023-09-13 14:01:44 +0530

    first commit

---
+++ src/Components/Popular.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../context/global'
+import styled from 'styled-components'
+import Sidebar from './Sidebar'
+
+function Popular({rendered}) {
+    const {popularAnime,isSearch, searchResults} = useGlobalContext()
+
+    const conditionalRender = () => {
+        if(!isSearch && rendered === 'popular'){
+            return popularAnime?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }else{
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                </Link>
+            })
+        }
+    }
+
+    return (
+        <PopularStyled>
+            <div className="popular-anime">
+                {conditionalRender()}
+            </div>
+            <Sidebar />
+        </PopularStyled>
+    )
+}
+
+const PopularStyled = styled.div`
+    display: flex;
+    .popular-anime{
+        margin-top: 2rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        padding-left: 5rem;
+        padding-right: 0;
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 2rem;
+        background-color: #fff;
+        border-top: 5px solid #e5e7eb;
+        a{
+            height: 500px;
+            border-radius: 7px;
+            border: 5px solid #e5e7eb;
+        }
+        a img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+    }
+    @media screen and (max-width: 768px) {
+        .popular-anime {
+          padding-left: 1rem;
+          grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+          a {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 0.5rem;
+            text-align: center;
+            .anime-name {
+              font-size: 1rem;
+              font-weight: 600;
+              color: #454e56;
+            }
+          }
+        }
+      }
+`;
+
+export default Popular
